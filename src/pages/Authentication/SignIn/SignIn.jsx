a85002a0ee1e41ab410b2b0842045f781cca06be
Jayammo <37230474+Jayammo@users.noreply.github.com>
2023-03-05 19:01:55 -0500

    style the Auth Component (#18)

--- src/pages/Authentication/SignIn/SignIn.jsx
+++ src/pages/Authentication/SignIn/SignIn.jsx
@@ -1,5 +1,4 @@
 import {
-	Card,
 	CardContent,
 	Typography,
 	CardHeader,
@@ -10,10 +9,10 @@ import FormTextField from '../../../components/UI/Form/FormTextField';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { SignInYupSchema } from '../AuthYupSchema';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { SetToken } from '../../../helper/CookieHandler';
 import { useAuth } from '../../../contexts/AuthProvider';
-import { StyledBox, StyledCardActions } from '../StyledAuth';
+import { StyledCardActions, StyledCard } from '../StyledAuth';
 import { useState } from 'react';
 
 const SignIn = () => {
@@ -46,36 +45,37 @@ const SignIn = () => {
 	};
 
 	return (
-		<StyledBox>
-			<Card raised={true}>
-				<CardHeader
-					title={
-						<Typography variant='h4' align='center'>
-							Sign In
-						</Typography>
-					}
-				/>
-				<form onSubmit={handleSubmit(onSubmit)}>
-					<CardContent>
-						<FormTextField control={control} name='username' label='Username' />
+		<StyledCard raised={true}>
+			<CardHeader
+				title={
+					<Typography variant='h4' align='center'>
+						Sign In
+					</Typography>
+				}
+			/>
+			<form onSubmit={handleSubmit(onSubmit)}>
+				<CardContent>
+					<FormTextField control={control} name='username' label='Username' />
 
-						<FormTextField
-							control={control}
-							name='password'
-							label='Password'
-							type='password'
-						/>
-					</CardContent>
-					<StyledCardActions>
-						{loading ? (
-							<CircularProgress />
-						) : (
-							<Button type='submit'>Submit</Button>
-						)}
-					</StyledCardActions>
-				</form>
-			</Card>
-		</StyledBox>
+					<FormTextField
+						control={control}
+						name='password'
+						label='Password'
+						type='password'
+					/>
+				</CardContent>
+				<StyledCardActions>
+					<Typography className='register' variant='subtitle1'>
+						<Link to='/registration'>Click Here</Link> to Register
+					</Typography>
+					{loading ? (
+						<CircularProgress />
+					) : (
+						<Button type='submit'>Submit</Button>
+					)}
+				</StyledCardActions>
+			</form>
+		</StyledCard>
 	);
 };
 
